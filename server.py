edceaf699fd344da1fa87458c284c7f385622f52
kspi <k.spiridonovas@gmail.com>
2016-03-06 14:04:42 +0200

    dropna

--- server.py
+++ server.py
@@ -137,6 +137,7 @@ class Arduino:
         end = float(end)
 
         log = pandas.read_csv(self.log_filename(name), sep=' ', names=('time', 'value'))
+        log = log.dropna()
         log.time = log.time.astype(float)
         log.value = log.value.astype(int)
         log = log[(start <= log.time) & (log.time <= end)]
