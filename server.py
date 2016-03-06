cf2ad4753cfcb48513517aaeb214bd2ff1da07fd
kspi <k.spiridonovas@gmail.com>
2016-03-06 11:29:14 +0200

    wind_fir name fix

--- server.py
+++ server.py
@@ -106,8 +106,8 @@ class Arduino:
         if key == 'door':
             return int(100 * (value - DOOR_CLOSED_POSITION) / (DOOR_OPEN_POSITION - DOOR_CLOSED_POSITION))
         if key == 'wind':
-            wind_fir.pop(0)
-            wind_fir.append(int(value))
+            self.wind_fir.pop(0)
+            self.wind_fir.append(int(value))
             return int(numpy.mean(a))
         return int(value)
 
