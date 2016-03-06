a503abc1c3b8d9e1c2c019e87972746733666d39
kspi <k.spiridonovas@gmail.com>
2016-03-06 11:46:41 +0200

    wind hysteresis

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ WINDOW_CLOSED_POSITION = 0
 DOOR_OPEN_POSITION = -9500
 DOOR_CLOSED_POSITION = 0
 WIND_THRESHOLD = 200
-WIND_MEAN_TIME = 10
+WIND_MEAN_TIME = 20
 
 ROOT = os.path.dirname(os.path.realpath(__file__))
 WEBROOT = os.path.join(ROOT, "webroot")
@@ -47,9 +47,10 @@ class Arduino:
             f.write("{} {}\n".format(time.time(), value))
 
     def handle(self):
-        if self.state.get('wind', 0) > WIND_THRESHOLD:
+        wind = self.state.get('wind', 0)
+        if wind > WIND_THRESHOLD:
             self.put('window', 0)
-        elif self.state.get('window', 100) != 100:
+        elif wind < WIND_THRESHOLD / 2 and self.state.get('window', 100) != 100:
             self.put('window', 100)
 
     def interact(self):
